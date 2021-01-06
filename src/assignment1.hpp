bc6e04a381655fb84bf766233b2812d815180a79
Mardava Gubbi <mrdv.rjgpl@gmail.com>
2021-01-06 16:31:46 -0500

    Add header guard to file

--- src/assignment1.hpp
+++ src/assignment1.hpp
@@ -1,6 +1,9 @@
+#ifndef ASSIGNMENT_1_HPP
+#define ASSIGNMENT_1_HPP
 
 #include <tf/transform_datatypes.h>
 
-tf::Point ForwardKinematics( double q1, double q2, double q3 );
-void Jacobian( double q1, double q2, double q3, double J[3][3] );
+tf::Point ForwardKinematics(double q1, double q2, double q3);
+void Jacobian(double q1, double q2, double q3, double J[3][3]);
 
+#endif /* ASSIGNMENT_1_HPP */
